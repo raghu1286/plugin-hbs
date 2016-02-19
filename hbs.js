8c1c2f57c452acb4cfb8b6e2054b1192a3dbdeec
Raghavendra Muppalla <raghavendra@cloudthing.com>
2016-02-19 13:01:57 +0530

    Made it es5 compatible

--- hbs.js
+++ hbs.js
@@ -4,7 +4,7 @@ var Handlebars = require('handlebars');
 
 var handlebarsRuntimePath = System.normalizeSync('handlebars/handlebars.runtime', __moduleName);
 
-export function translate(load) {
+module.exports = function translate(load) {
   var precompiled = Handlebars.precompile(load.source);
   load.source = `module.exports = require('${handlebarsRuntimePath}').template(${precompiled});`;
 }
