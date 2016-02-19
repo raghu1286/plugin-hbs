829554a34aa7cb5bd655352510be13e9f15988d9
Raghavendra Muppalla <raghavendra@cloudthing.com>
2016-02-19 13:03:42 +0530

    Made it es5 compatible

--- hbs.js
+++ hbs.js
@@ -1,10 +1,19 @@
 'use strict';
 
-var Handlebars = require('handlebars');
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+exports.translate = translate;
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _handlebars = require('handlebars');
+
+var _handlebars2 = _interopRequireDefault(_handlebars);
 
 var handlebarsRuntimePath = System.normalizeSync('handlebars/handlebars.runtime', __moduleName);
 
-module.exports = function translate(load) {
-  var precompiled = Handlebars.precompile(load.source);
-  load.source = `module.exports = require('${handlebarsRuntimePath}').template(${precompiled});`;
+function translate(load) {
+  var precompiled = _handlebars2['default'].precompile(load.source);
+  load.source = 'module.exports = require(\'' + handlebarsRuntimePath + '\').template(' + precompiled + ');';
 }
